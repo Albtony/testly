084ba92d36c58fe3fb63c8f1d9b32388f5f23093
Nuzul Abatony <albtony21@gmail.com>
2025-05-11 12:33:36 +0700

    feat: Add initial Spring Boot project structure

---
+++ src/test/java/com/example/testly/TestlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
